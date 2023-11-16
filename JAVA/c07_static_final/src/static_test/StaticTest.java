cd26d42dbceacbee39e03d85d82eeb2320128c34
nanaball <nanaball127@gmail.com>
2023-11-16 15:51:55 +0900

    _231116_static_public

---
+++ JAVA/c07_static_final/src/static_test/StaticTest.java
@@ -0,0 +1,37 @@
+package static_test;
+
+public class StaticTest {
+
+	static String test;
+	
+	public static void main(String[] args) {
+	
+		System.out.println(test);
+		
+		
+		Student.school = "부산 아이티 아카데미";
+		System.out.println(Student.school);
+		// Student.name = "최기근";
+		Student choi = new Student();
+		choi.name = "최기근";
+		choi.age = 18;
+		choi.grade = "4";
+		choi.school = "부산 직업 전문학교";
+		
+		Student lee = new Student();
+		System.out.println(lee.school);
+		
+		Student song = new Student();
+		song.school = "부산교도소";
+		
+		System.out.println(Student.school);
+		
+		int result = Student.sum(1, 2);
+		System.out.println(result);
+		
+		// Student.setAge(20);
+		song.setAge(30);
+		
+	} // end main
+	
+} // end class
